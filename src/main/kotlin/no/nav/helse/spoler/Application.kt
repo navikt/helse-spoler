bdf4816add90ca8d891d90df449368eaf1417455
Erlend Viddal <eviddal@gmail.com>
2021-07-01 16:26:37 +0200

    Fikset den teisteste feilen. Co-authored-by: Peter Stensby <peter.stensby@nav.no>

--- src/main/kotlin/no/nav/helse/spoler/Application.kt
+++ src/main/kotlin/no/nav/helse/spoler/Application.kt
@@ -24,8 +24,8 @@ fun main() {
 
 private fun spol(env: Map<String, String>) {
     Thread.setDefaultUncaughtExceptionHandler { _, throwable -> log.error(throwable.message, throwable) }
-        Spol(env, "tbd.rapid.v1", "tbd-spaghet-v1", LocalDateTime.of(2021, 6, 25,23,0,0))
     listOf<Spol>(
+        Spol(env, "tbd.rapid.v1", "tbd-spaghet-v1", LocalDateTime.of(2021, 6, 25,23,0,0))
     ).forEach(Spol::spol)
 }
 
